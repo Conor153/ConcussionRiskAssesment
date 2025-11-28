59320f51775b9db3fba9bb47a4f71117bfe2fa13
Conor Callaghan <157692610+Conor153@users.noreply.github.com>
2025-11-28 18:28:01 +0000

    Add YOLO model files and video processing script
    Added YOLO model weights (yolo11n.pt, yolo11s.pt, yolo11n-pose.pt) to the backend and project root. Updated utilities.py to process video frames using the YOLO model, display annotated frames, and include a frame rescaling function.

--- backend/utilities.py
+++ backend/utilities.py
@@ -1,5 +1,40 @@
 import cv2 as cv
 import numpy as np
-img = cv.imread("")
-cv.imshow("Display window", img)
-k = cv.waitKey(0)
+from ultralytics import YOLO
+model = YOLO('yolo11n.pt')
+
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+    #Resize frame to a set dimension
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+# Read Image
+#img = cv.imread('C:\Users\Conor\Videos\ConcussionAssessment\Concussion Hits\CJStroudConcussion.mp4')
+# Show Image
+#Display Window
+#cv.imshow("Display window", img)
+#cv.waitKey(0)
+
+#Read Video
+capture = cv.VideoCapture('C:/Users/Conor/Videos/ConcussionAssessment/Concussion Hits/CJStroudConcussion.mp4')
+#While loop to read video 
+#Frame by Frame
+while True:
+    isTrue, frame = capture.read()
+    results = model(frame)
+    annotated_frame = results[0].plot()
+
+    frameResized = rescaleFrame(annotated_frame )
+    #Display every frame
+    #cv.imshow("Video", frame)
+    cv.imshow("Video Resized", frameResized)
+    #Stop the video~ If d is pressed
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
+
+
+
